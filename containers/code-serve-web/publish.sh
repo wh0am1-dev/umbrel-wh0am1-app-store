cbb9032505469345ad66b2b8e06b1857f66b28a3
Carlos Aguilar <hey@carlos-aguilar.com>
2026-03-03 03:34:27 +0100

    refactor: rename to code-serve

--- containers/code-serve-web/publish.sh
+++
@@ -1,1 +0,0 @@
-docker buildx build --push --platform linux/amd64,linux/arm64 -t wh0am1dev/code-serve-web:<version> .
---
+++ containers/code-serve/publish.sh
@@ -0,0 +1,1 @@
+docker buildx build --push --platform linux/amd64,linux/arm64 -t wh0am1dev/code-serve:<version> .
